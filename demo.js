d4840892e13dbdf054b26b16ebc0c4c8a443e55f
sumit-zenika <46732797+sumit-zenika@users.noreply.github.com>
2019-01-21 15:46:59 +0800

    Create demo.js

---
+++ demo.js
@@ -0,0 +1,12 @@
+{
+    "js": {
+        "1":  { 
+            "q": "What is Javascript?", 
+            "ans": "JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm." 
+        },
+        "2":  { 
+            "q": "What is Javascript?", 
+            "ans": "JavaScript, often abbreviated as JS, is a high-level, interpreted programming language that conforms to the ECMAScript specification. It is a language that is also characterized as dynamic, weakly typed, prototype-based and multi-paradigm." 
+        }
+    }
+}
